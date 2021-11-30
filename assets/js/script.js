b3a5b238f3f4dac3a3eb6faefc056ed52613d112
Michelle <mchristine.stone91@gmail.com>
2021-11-29 20:18:23 -0500

    added CSS to both files

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//DOM Elements
+var searchBarEl = document.querySelector(".movie-input");
+var searchButtonEl = document.querySelector(".btn");
+
+// event Listener for search button
+searchButtonEl.addEventListener("click", function () {
+  var apiURl = "";
+});
