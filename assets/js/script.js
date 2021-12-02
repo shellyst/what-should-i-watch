30aa47d1fba71fd1fdb6dfd717860241d3f7fae1
Michelle <mchristine.stone91@gmail.com>
2021-12-02 11:52:13 -0500

    fixed modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,43 @@ function displayMovies(movieName) {
   var apiURl = "https://www.omdbapi.com/?s=" + movieName + "&apikey=12490b6e";
   fetch(apiURl)
     .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          //delete later!!!
-          console.log("API is working!", data);
-          moviesAPI(data);
-        });
-      } else {
-        alert("Please enter a valid movie!");
-      }
-    })
+      return response.json().then(function (data) {
+        //delete later!!!
+        console.log("API is working!", data);
 
+        if (data.Response === "False") {
+          console.log("not working");
+          // Get the modal
+          var modal = document.getElementById("myModal");
+
+          // Get the button that opens the modal
+          var btn = document.getElementById("myBtn");
+
+          // Get the <span> element that closes the modal
+          var span = document.getElementsByClassName("close")[0];
+
+          // modal displays if condition is false
+          modal.style.display = "block";
+
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function () {
+            modal.style.display = "none";
+          };
+
+          // When the user clicks anywhere outside of the modal, close it
+          window.onclick = function (event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          };
+        } else {
+          moviesAPI(data);
+        }
+      });
+    })
     .catch(function (error) {
       // Notice this `.catch()` getting chained onto the end of the `.then()`
-      alert("Unable to connect to OMBD API");
+      console.log("Unable to connect to OMBD API");
     });
 }
 
