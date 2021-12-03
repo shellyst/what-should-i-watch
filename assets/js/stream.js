8eefd115a3677f2ed81f351d3695903ed27a252f
BaqerGhaddar <mohammedghaddar2019@gmail.com>
2021-12-03 00:46:11 -0500

    stream.js

--- assets/js/stream.js
+++ assets/js/stream.js
@@ -1,47 +1,3 @@
-// var companies = [
-//     {
-//         name: "HBO Max",
-//         logo: "./assets/images/hbo.png",
-//         starting_price: "$9.99 per month with ads",
-//         simultaneous_streams: "3 Live",
-//         tv: "No",
-//         exclusive_originals: "Yes",
-//         platforms_and_devices: "Amazon Fire, Roku, Android, Android TV, Apple TV, Chromecast, iPhone, iPad, iPod touch, PlayStation 4, Macs and PCs, Samsung Smart TV, Xbox One, TiVo",
-//         website: "https://www.hbomax.com"
-//     },
-//     {
-//         name: "Netflix",
-//         logo: "./assets/images/netflix.png",
-//         starting_price: "$9 per month with ads",
-//         simultaneous_streams: "1-4",
-//         tv: "no",
-//         exclusive_originals: "yes",
-//         platforms_and_devices: "Android, iOS, web browers, Apple TV, Chromecast, Fire TV, Nvidia Shield, Roku, Smart TVs, PS4, Xbox,set top boxes",
-//         website: "https://www.netflix.com/"
-//     },
-//     {
-//         name: "Disney+",
-//         logo: "./assets/images/disney.png",
-//         starting_price: "$8 per month with ads",
-//         simultaneous_streams: "4",
-//         tv: "no",
-//         exclusive_originals: "yes",
-//         platforms_and_devices: "Amazon Fire TV and Fire tablets, Roku, Chromecast, iOS, iPadOS,Android, Android TV, Apple TV, Xbox, PlayStation 4, Android-based Sony Smart TVs, Chromebooks, Samsung and LG devices",
-//         website: "https://www.disneyplus.com/"
-//     },
-//     {
-//         name: "Hulu",
-//         logo: "https://via.placeholder.com/150",
-//         starting_price: "	$6.99 per month; $64.99 per month",
-//         simultaneous_streams: "	2 or Unlimited (at home) and 3 (mobile devices)",
-//         tv: "yes",
-//         exclusive_originals: "yes",
-//         platforms_and_devices: "Android, iOS, web browers, Apple TV, Chromecast, Fire TV, Nvidia Shield, Roku, Smart TVs, PS4, Xbox,set top boxes",
-//         website: "https://www.hulu.com/"
-//     }
-// ]
-
-
 function generateServiceCard (data) {
     return `<article class="service">
     <div class="stream-bio">
@@ -100,12 +56,6 @@ function searchCompany () {
 
      servicesDiv.innerHTML = articles;
    }) 
-   
-    // for (let i = 0; i < companies.length; i++) {
-    //     if(companies[i].name == streamInput.value) {
-    //         articles+=generateServiceCard(companies[i]);
-    //     }
-    // }
    
 }
 
