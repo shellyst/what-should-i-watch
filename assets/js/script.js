d3686ebe19e9ecc8c9d44f69de5be7c3313e8182
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2021-12-04 11:02:41 -0500

    second API added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,61 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
+const additionalInfoAPI = function (data) {
+  console.log("hey vlad you dumd dumb", isArray(data));
+  for (var i = 0; i < data.Search.length; i++) {
+    console.log("hey stoneMichelle this is i-", imdbID);
+    "https://www.omdbapi.com/?i=" + imdbID + "&apikey=12490b6e";
+    fetch(additionalInfoAPI)
+      .then(function (response) {
+        console.log("im working");
+        return response.json().then(function (data) {
+          console.log(data);
+
+          if (data.Response === "False") {
+            console.log("not working");
+            // Get the modal
+            var modal = document.getElementById("myModal");
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("myBtn");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // modal displays if condition is false
+            modal.style.display = "block";
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+              modal.style.display = "none";
+            };
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+              }
+            };
+          } else {
+            movieInfoEl(data);
+          }
+        });
+      })
+      .catch(function (error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()`
+        console.log("Unable to connect to OMBD API");
+        console.log(error.message);
+      });
+  }
+};
+
+var movieInfoEl = function (data) {
+  var nameDirector = document.createElement("p");
+  nameDirector.textContent = "hello";
+  movieContainer.append(nameDirector);
+};
+
 // event Listener for search button
 function displayMovies(movieName) {
   var apiURl = "https://www.omdbapi.com/?s=" + movieName + "&apikey=12490b6e";
@@ -15,7 +70,9 @@ function displayMovies(movieName) {
       return response.json().then(function (data) {
         //delete later!!!
         console.log("API is working!", data);
-
+        console.log(data.Search.imdbID);
+        console.log("vlad the data works", data);
+        // confirm
         if (data.Response === "False") {
           console.log("not working");
           // Get the modal
@@ -42,7 +99,9 @@ function displayMovies(movieName) {
             }
           };
         } else {
-          moviesAPI(data);
+          console.log("else statement", data);
+          // moviesAPI(data);
+          additionalInfoAPI(data);
         }
       });
     })
@@ -57,15 +116,13 @@ searchButtonEl.addEventListener("click", function () {
   storesearch(searchBarEl.value);
 });
 
-function storesearch(whattostore){
-
-  localStorage.search=whattostore;
+function storesearch(whattostore) {
+  localStorage.search = whattostore;
   document.getElementById("past-search_1").innerHTML = localStorage.search;
   document.getElementById("past-search_2").innerHTML = localStorage.search1;
   document.getElementById("past-search_3").innerHTML = localStorage.search2;
-  localStorage.search2=localStorage.search1;
-  localStorage.search1=localStorage.search;
- 
+  localStorage.search2 = localStorage.search1;
+  localStorage.search1 = localStorage.search;
 }
 
 var moviesAPI = function (data) {
@@ -109,3 +166,4 @@ var moviesAPI = function (data) {
   searchBarEl.value = "";
 };
 
+// movie description and info api
