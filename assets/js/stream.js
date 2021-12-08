d394dcde6c704a2cbd51510e352593cf6f47890c
victoriatsitskishvili <89715481+victoriatsitskishvili@users.noreply.github.com>
2021-12-07 19:36:19 -0500

    Update stream.js
    Changes to include modals to work

--- assets/js/stream.js
+++ assets/js/stream.js
@@ -37,13 +37,38 @@ function searchCompany() {
   var url = `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${streamInput.value}&country=uk&rapidapi-key=8e00451b55msh53c30d28f98049dp1c6ed2jsne4c2428dcf54`;
   console.log(url);
   $.getJSON(url, function (data) {
-    var locations = data.results[0].locations;
-    for (let i = 0; i < locations.length; i++) {
-      var card = makeServiceCard(locations[i]);
-      articles += card;
-    }
-    console.log(articles);
-    servicesDiv.innerHTML = articles;
+      console.log(data);
+      console.log(data.results.length)
+      if (data.results.length == undefined) {
+          console.log('error');
+          // Get the modal
+          var modal = document.getElementById("myModal");
+          // Get the button that opens the modal
+          var btn = document.getElementById("myBtn");
+          // Get the <span> element that closes the modal
+          var span = document.getElementsByClassName("close")[0];
+          // modal displays if condition is false
+          modal.style.display = "block";
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function () {
+            modal.style.display = "none";
+          };
+          // When the user clicks anywhere outside of the modal, close it
+          window.onclick = function (event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }
+          };
+      } else {
+          var locations = data.results[0].locations;
+          for (let i = 0; i < locations.length; i++) {
+            var card = makeServiceCard(locations[i]);
+            articles += card;
+          }
+          console.log(articles);
+          servicesDiv.innerHTML = articles;
+
+      }
   });
 }
 
